e3a999809eca70127cace1cbcabe8dbc661d2aa3
kdivelbiss <kdivelbiss@aainsco.com>
2024-01-09 11:30:57 -0500

    Remove answers

--- PairProgramming/Testing/OurListTest.cs
+++ PairProgramming/Testing/OurListTest.cs
@@ -5,27 +5,11 @@
         [Fact]
         public void AddingFiveItemsShouldReturnACountOfFive()
         {
-            var list = new OurList();
-            for (int i = 0; i < 5; i++)
-            {
-                list.Add(i);
-            }
-
-            Assert.Equal(5, list.Count);
         }
 
         [Fact]
         public void ClearShouldResultInEmptyList()
         {
-            var list = new OurList();
-            for (int i = 0; i < 5; i++)
-            {
-                list.Add(i);
-            }
-
-            list.Clear();
-
-            Assert.Equal(0, list.Count);
         }
     }
 }
